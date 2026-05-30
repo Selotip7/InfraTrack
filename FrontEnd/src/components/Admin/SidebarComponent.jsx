403cf20f921b5a7a7febf66abdb9ee8603fd9872
Selotip7 <selotip7@gmail.com>
2026-05-30 19:48:58 +0700

    feat: create side nav

--- FrontEnd/src/components/Admin/SidebarComponent.jsx
+++ FrontEnd/src/components/Admin/SidebarComponent.jsx
@@ -1,21 +1,90 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHouse,faFileLines,faMap} from '@fortawesome/free-regular-svg-icons'
+import {faChartLine} from "@fortawesome/free-solid-svg-icons"
 export default function Sidebar(){
+  const linkClass =
+    "flex items-center gap-3 px-3 py-2 rounded hover:bg-gray-100";
+
   return (
     <>
-      <div className="flex flex-col w-56 h-screen bg-primary text-black ">
+      <styles></styles>
+      <div className="flex flex-col w-56 h-screen bg-primary-admin text-white ">
         <div
           id="brand"
-          className="relative flex h-16 items-center gap-3 px-3 after:absolute after:bottom-0 after:left-1/2 after:h-[2px] after:w-[90%] after:-translate-x-1/2 after:bg-amber-950"
+          className="relative flex h-[90px] items-center gap-3 px-3 after:absolute after:bottom-0 after:left-1/2 after:h-[2px] after:w-[90%] after:-translate-x-1/2 after:bg-white"
         >
-          <div id="logo" className="w-1/4 ">
+          <div id="logo" className="w-[50px] ">
             <img src={logo} className="rounded-[5px]" />
           </div>
-          <div id="brand-text" className=" flex flex-col gap-0 m-0">
+          <div id="brand-text" className=" flex flex-col ">
             <p className="font-medium leading-5 text-[20px]">InfraTrack</p>
             <p className="text-[12px]">Monitoring System</p>
           </div>
         </div>
+        <nav className="mt-5">
+          <ul className="space-y-2 px-2">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${linkClass} ${
+                    isActive ? "bg-primary text-white" : "hover:bg-gray-100"
+                  }`
+                }
+              >
+                <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${linkClass} ${
+                    isActive ? "bg-primary text-white" : "hover:bg-gray-100"
+                  }`
+                }
+              >
+                
+                  <FontAwesomeIcon icon={faFileLines}></FontAwesomeIcon>
+                  <span>Reports</span>
+                
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${linkClass} ${
+                    isActive ? "bg-primary text-white" : "hover:bg-gray-100"
+                  }`
+                }
+              >
+                
+                  <FontAwesomeIcon icon={faMap}></FontAwesomeIcon>
+                  <span>Map monitoring</span>
+               
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${linkClass} ${
+                    isActive ? "bg-primary text-white" : "hover:bg-gray-100"
+                  }`
+                }
+              >
+                
+                  <FontAwesomeIcon icon={faChartLine}></FontAwesomeIcon>
+                  <span>Analytics</span>
+                
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
